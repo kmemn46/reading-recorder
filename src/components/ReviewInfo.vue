332c23f56e80246dbf03029b9a7360b7e478c6d7
kmemn46 <ikamidev@gmail.com>
2021-09-08 17:06:23 +0900

    #3 対応

--- src/components/ReviewInfo.vue
+++ src/components/ReviewInfo.vue
@@ -1,20 +1,53 @@
 <template>
-  <v-card class="ma-2" elevation="6" :class="{ linkable }" @click="onclick">
-    <v-list-item two-line>
-      <v-list-item-avatar  tile left size="100"><img :src="book.image" /></v-list-item-avatar>
+  <v-card class="ma-2" elevation="6" :class="{ linkable }">
+    <v-list-item three-line>
+      <v-list-item-avatar  tile left size="80"><img :src="book.image" /></v-list-item-avatar>
       <v-list-item-content>
-        <v-list-item-title class="headline mb-1">
+        <div class="text-overline mb-4">
+          <v-row align="center" class="mx-0">
+          <v-rating
+            :value= book.rating 
+            color="amber"
+            dense
+            readonly
+            size="20"
+          ></v-rating>
+          <div class="grey--text ms-4">
+            {{ book.read}}
+          </div>
+          </v-row>
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
           {{ book.title }}
         </v-list-item-title>
         <v-list-item-subtitle>{{ book.memo }}</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
+    <!-- ボタン群 -->
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+        color="cyan"
+        @click="onclickEdit"
+      ><v-icon>mdi-pencil</v-icon>
+      </v-btn>
+      <v-btn
+        outlined
+        rounded
+        text
+        color="primary"
+        @click="onclickDel"
+      ><v-icon>mdi-delete</v-icon>
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
-import { UPDATE_CURRENT } from '@/mutation_types'
+import { UPDATE_CURRENT, DELETE_BOOK } from '@/mutation_types'
 
 export default {
   name: 'review-info',
@@ -25,13 +58,17 @@ export default {
   },
   methods: {
     // UPDATE_CURRENTアクションを同盟のメソッドに紐づけ
-    ...mapActions([UPDATE_CURRENT]),
-    // クリック時に現在の書籍情報をステートに保存andフォームに移動
-    onclick() {
+    ...mapActions([UPDATE_CURRENT, DELETE_BOOK]),
+    // 編集クリック時に現在の書籍情報をステートに保存andフォームに移動
+    onclickEdit() {
       if (this.linkable) {
         this[UPDATE_CURRENT](this.book)
         this.$router.push('/form')
       }
+    },
+    onclickDel() {
+      this[DELETE_BOOK](this.book)
+      this.$router.go({path: this.$router.currentRoute.path, force: true})
     }
   }
 }
@@ -40,7 +77,7 @@ export default {
 <style scoped>
 .linkable:hover {
     cursor: pointer;
-    background-color: #f7eb67;
+    background-color: #f5ed9b;
 }
 
 .image {
